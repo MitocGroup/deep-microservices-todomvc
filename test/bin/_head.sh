ddc1e2b9316c383ccc4167d3d3e916c0cf458ae8
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-02 17:00:52 +0300

    Add travis_ci

---
+++ test/bin/_head.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+__SCRIPT_PATH=$(cd $(dirname $0); pwd -P)
+
+__ROOT_PATH="${__SCRIPT_PATH}/../../"
+
+__COVERAGE_PATH=${__SCRIPT_PATH}"/../coverage/"
+
+__DEEPNGROOT_PATH="${__ROOT_PATH}src/DeepNgRoot/Frontend/js/"
+__DEEPNGTODO_PATH="${__ROOT_PATH}src/DeepNgToDo/Frontend/js/"
+
+subpath_run_cmd () {
+    local DIR
+    local CMD
+    local EXPR
+
+    DIR=$(cd $1 && pwd -P)
+    CMD=$2
+
+    cd $DIR/${subpath} && eval_or_exit "$CMD"
+}
+
+eval_or_exit() {
+    local RET_CODE
+
+    eval "$1"
+    RET_CODE=$?
+
+    if [ ${RET_CODE} == 0 ]; then
+        echo "[SUCCEED] $1"
+    else
+        echo "[FAILED] $1"
+        exit 1
+    fi
+}
