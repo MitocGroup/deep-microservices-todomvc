2a5057d558e3626ee71e401d99e2d8a80c639fec
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-04 19:02:41 +0300

    Combining test

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -8,12 +8,14 @@ istanbul-combine -d ${__COVERAGE_PATH} -r lcov -p both \
   ${__ROOT_PATH}src/DeepNgRoot/Tests/Frontend/coverage/*/coverage-final.json \
   ${__ROOT_PATH}src/DeepNgToDo/Tests/Frontend/coverage/*/coverage-final.json
 
+echo "Done combining"
 ### Upload Coverage info to Codacy and to Coveralls ###
 
 cat ${__COVERAGE_PATH}"lcov.info" | codacy-coverage
 
 cat ${__COVERAGE_PATH}"lcov.info" | coveralls
 
+echo "Done uploading"
 
 #### Cleanup coverage! ###
 #__CMD='rm -rf ./test/coverage'
