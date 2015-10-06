f56403fdbc8bc58ad92f3580cf1a7b73891895fb
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-07 01:09:38 +0300

    Test coverage

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -11,6 +11,8 @@ istanbul-combine -d ${__COVERAGE_PATH} -r lcov -p both \
   ${__SRC_PATH}*/Tests/Frontend/coverage/*/*.json \
   ${__SRC_PATH}*/Tests/Backend/coverage/*.json
 
+
+
 ### Upload Coverage info to Codacy ###
 cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
 cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
