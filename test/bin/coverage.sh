3a497897ed2ece63179ccb9655573ab5595d87d2
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-16 18:51:06 +0300

    test coveralls for public repos

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -7,7 +7,7 @@ __CMD='npm run coverage'
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
 
 ### Merge Coverage results ###
-istanbul-combine -d ${__COVERAGE_PATH} -r lcov -p both \
+istanbul-combine -d ${__COVERAGE_PATH} -r lcovonly -p both \
   ${__SRC_PATH}*/Tests/Frontend/coverage/*/coverage-final.json \
   ${__SRC_PATH}*/Tests/Backend/coverage/*.json
 
