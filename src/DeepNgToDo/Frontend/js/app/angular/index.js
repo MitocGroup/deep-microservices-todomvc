ce3787e0dcb86728261e6799fad71d22f3492006
cypher-m <mponomariov@pycoding.biz>
2015-10-30 12:22:42 +0200

    Add missing semicolons and package.json

--- src/DeepNgToDo/Frontend/js/app/angular/index.js
+++ src/DeepNgToDo/Frontend/js/app/angular/index.js
@@ -3,10 +3,10 @@
 
 import moduleName from './name';
 
-import '../../../node_modules/todomvc-common/base.css!'
-import '../../../node_modules/todomvc-app-css/index.css!'
-import '../../../node_modules/todomvc-common/base.js'
-import _ from '../../../node_modules/underscore/underscore'
+import '../../../node_modules/todomvc-common/base.css!';
+import '../../../node_modules/todomvc-app-css/index.css!';
+import '../../../node_modules/todomvc-common/base.js';
+import _ from '../../../node_modules/underscore/underscore';
 import './module/index';
 import './controllers/index';
 import './services/index';
