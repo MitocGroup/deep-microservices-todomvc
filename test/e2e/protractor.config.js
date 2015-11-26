7d093bf27b4fdfb11d5fce7ee03fa8070f0a20b9
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-26 22:56:27 +0200

    Add SauceLab configuration

--- test/e2e/protractor.config.js
+++ test/e2e/protractor.config.js
@@ -1,5 +1,4 @@
-exports.config = {
-
+var config = {
     seleniumAddress: 'http://localhost:4444/wd/hub/',
 
     capabilities: {
@@ -18,13 +17,18 @@ exports.config = {
         // If true, include stack traces in failures.
         includeStackTrace: true,
         // Default time to wait in ms before a test fails.
-        defaultTimeoutInterval: 30000,
-    },
-
-    mochaOpts:{
-        reporter:'spec',
-        slow: 30000,
-        enableTimeouts: false
+        defaultTimeoutInterval: 50000,
     },
 };
+if (process.env.TRAVIS_BUILD_NUMBER) {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER,
+        'name': 'App Tests'
+    };
+}
 
+exports.config = config;
