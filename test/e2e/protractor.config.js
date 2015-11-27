70cf44db506967ec1b101f1f2182d02d86c2d26a
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-27 11:57:46 +0200

    Fix browser configuration

--- test/e2e/protractor.config.js
+++ test/e2e/protractor.config.js
@@ -2,40 +2,47 @@ exports.config = {
 
   sauceUser: process.env.SAUCE_USERNAME,
   sauceKey: process.env.SAUCE_ACCESS_KEY,
-  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
   build: process.env.TRAVIS_BUILD_NUMBER,
 
   //it is not working for Sauce Labs
-  //seleniumAddress: 'http://localhost:4445/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
 
   specs: ['./Tests/*.spec.js'],
 
   multiCapabilities: [
     {
-      name: 'Chrome test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      name: 'Chrome on Linux test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'chrome',
-      seleniumVersion: '2.47.0',
+      //seleniumVersion: '2.47.0',
     },
     {
-      name: 'Firefox test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      name: 'Firefox  on Linux test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'firefox',
-      seleniumVersion: '2.47.0',
+      //seleniumVersion: '2.46.0',
     },
     {
-      name: 'IE test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      name: 'IE on win7 test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'internet explorer',
-      seleniumVersion: '2.47.0',
+      //seleniumVersion: '2.46.0',
     },
     {
-      name: 'Safari test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      name: 'Safari on iPhone test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      browserName: 'Safari',
       platformName: "iOS",
       platformVersion: "7.1",
       deviceName: "iPhone Simulator",
-      seleniumVersion: '2.47.0',
+      //seleniumVersion: '2.47.0',
     },
     {
-      name: 'Android test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
-      browserName: 'android',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      name: 'Chrome on Google Nexus 7 test for build: ' + process.env.TRAVIS_BUILD_NUMBER,
+      deviceName: 'Google Nexus 7 HD Emulator',
+      browserName: 'Chrome',
+      // sharding tests, - very cool
       shardTestFiles: true,
       maxInstances: 5,
     },
