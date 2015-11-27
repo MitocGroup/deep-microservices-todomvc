c038ffbce464376b1970ef30fd0d87806422441e
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-27 08:43:48 +0200

    Test 2.47.0 selenium driver version

--- test/e2e/protractor.config.js
+++ test/e2e/protractor.config.js
@@ -1,54 +1,49 @@
 var config = {
 
-    sauceUser: process.env.SAUCE_USERNAME,
-    sauceKey: process.env.SAUCE_ACCESS_KEY,
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
 
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./Tests/*.spec.js'],
-    baseUrl: 'http://localhost:9001',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['./Tests/*.spec.js'],
+  baseUrl: 'http://localhost:8000',
 
-    //capabilities: {
-    //    'browserName': 'chrome',
-    //    'chromeOptions': {
-    //        args: ['--test-type']
-    //    }
-    //},
-
-    multiCapabilities: [{
+  multiCapabilities: [
+    {
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER,
       name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'firefox',
-      seleniumVersion: '2.47.0'
-    }, {
+      seleniumVersion: '2.47.0',
+    },
+    {
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER,
       name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'chrome',
-      seleniumVersion: '2.48.2',
+      seleniumVersion: '2.47.0',
       chromedriverVersion: '2.20',
-    }, {
+    },
+    {
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER,
       name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
       browserName: 'internet explorer',
-      seleniumVersion: '2.46.0'
-    }],
-
-    framework: 'jasmine2',
-
-
-    jasmineNodeOpts: {
-      // If true, display spec names.
-      isVerbose: true,
-      // If true, print colors to the terminal.
-      showColors: true,
-      // If true, include stack traces in failures.
-      includeStackTrace: true,
-      // Default time to wait in ms before a test fails.
-      defaultTimeoutInterval: 50000,
+      seleniumVersion: '2.47.0'
     }
-    ,
-  };
+  ],
+
+  framework: 'jasmine2',
+
+  jasmineNodeOpts: {
+    // If true, display spec names.
+    isVerbose: true,
+    // If true, print colors to the terminal.
+    showColors: true,
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 50000,
+  },
+};
 
 exports.config = config;
