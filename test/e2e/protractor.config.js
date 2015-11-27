db31256a17809837a7a928247510fd9cb7cf5be0
vcernomschi <vcernomschi@mitocgroup.com>
2015-11-27 07:47:56 +0200

    Update protractor configuration

--- test/e2e/protractor.config.js
+++ test/e2e/protractor.config.js
@@ -1,6 +1,12 @@
 var config = {
-    //seleniumAddress: 'http://localhost:4444/wd/hub/',
-    //
+
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['./Tests/*.spec.js'],
+    baseUrl: 'http://localhost:9001',
+
     //capabilities: {
     //    'browserName': 'chrome',
     //    'chromeOptions': {
@@ -9,37 +15,40 @@ var config = {
     //},
 
     multiCapabilities: [{
-        browserName: 'firefox',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
+      browserName: 'firefox',
+      seleniumVersion: '2.47.0'
     }, {
-        browserName: 'chrome',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
+      browserName: 'chrome',
+      seleniumVersion: '2.48.2',
+      chromedriverVersion: '2.20',
     }, {
-        browserName: 'internet explorer',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+      name: 'ng-pattern-restrict Chrome build ' + process.env.TRAVIS_BUILD_NUMBER,
+      browserName: 'internet explorer',
+      seleniumVersion: '2.46.0'
     }],
 
     framework: 'jasmine2',
 
-    specs: ['./Tests/*.spec.js'],
 
     jasmineNodeOpts: {
-        // If true, display spec names.
-        isVerbose: true,
-        // If true, print colors to the terminal.
-        showColors: true,
-        // If true, include stack traces in failures.
-        includeStackTrace: true,
-        // Default time to wait in ms before a test fails.
-        defaultTimeoutInterval: 50000,
-    },
-};
-if (process.env.TRAVIS_BUILD_NUMBER) {
-    config.sauceUser = process.env.SAUCE_USERNAME;
-    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
-    config.capabilities = {
-        'browserName': 'chrome',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-        'build': process.env.TRAVIS_BUILD_NUMBER,
-        'name': 'App Tests'
-    };
-}
+      // If true, display spec names.
+      isVerbose: true,
+      // If true, print colors to the terminal.
+      showColors: true,
+      // If true, include stack traces in failures.
+      includeStackTrace: true,
+      // Default time to wait in ms before a test fails.
+      defaultTimeoutInterval: 50000,
+    }
+    ,
+  };
 
 exports.config = config;
