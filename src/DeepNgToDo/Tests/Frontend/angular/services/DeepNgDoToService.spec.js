073f1fb2d20d32e44ab2794c12115f44ff3762f3
vcernomschi <vcernomschi@mitocgroup.com>
2015-12-09 22:22:23 +0200

    Update outdated unit test for createToDo()

--- src/DeepNgToDo/Tests/Frontend/angular/services/DeepNgDoToService.spec.js
+++ src/DeepNgToDo/Tests/Frontend/angular/services/DeepNgDoToService.spec.js
@@ -34,8 +34,8 @@ describe('Services', function() {
     expect(deepNgToDoService.allChecked).toBe(false);
   });
 
-  it('Check createToDo() method for !title returns false', function() {
-    expect(deepNgToDoService.createToDo()).toBe(false);
+  it('Check createToDo() method for !title returns promise', function() {
+    expect(deepNgToDoService.createToDo().constructor.name).toBe(false);
   });
 
   it('Check createToDo() with title returns promise with reponse.isError',
