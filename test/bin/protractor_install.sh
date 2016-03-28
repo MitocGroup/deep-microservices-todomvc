64f539362a3da536cfa2c0fe7cbfae92605a764b
vcernomschi <vcernomschi@mitocgroup.com>
2016-03-28 23:37:02 +0300

    Downgrade protractor till 2.0.x

--- test/bin/protractor_install.sh
+++ test/bin/protractor_install.sh
@@ -5,8 +5,8 @@ source $(dirname $0)/_head.sh
 ### Start protractor test ###
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
-if [ ${TRAVIS_NODE_VERSION} != '0.10' ]; then
-  npm install -g protractor@3.0.x
+if [ ${TRAVIS_NODE_VERSION} = '0.10' ] || [ ${TRAVIS_NODE_VERSION} = '0.11' ] || [ ${TRAVIS_NODE_VERSION} = '0.12' ]; then
+  npm install -g protractor@2.0.x
 else
   npm install -g protractor@3.0.x
 fi
