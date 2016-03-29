ab64658e63bf4bdb0cfde123ddd2e441a07403bf
vcernomschi <vcernomschi@mitocgroup.com>
2016-03-29 14:02:52 +0300

    Update logging

--- test/bin/posttest.sh
+++ test/bin/posttest.sh
@@ -3,8 +3,6 @@
 source $(dirname $0)/_head.sh
 
 ### Start protractor test ###
-echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
-
 if [ ${TRAVIS_NODE_VERSION} != '5.9' ]; then
   protractor ${__PROTRACTOR_CONFIG_PATH} --troubleshoot true
 fi
--- test/bin/protractor_install.sh
+++ test/bin/protractor_install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-source $(dirname $0)/_head.sh
-
-### Start protractor test ###
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
 if [ ${TRAVIS_NODE_VERSION} = '0.10' ] || [ ${TRAVIS_NODE_VERSION} = '0.11' ] || [ ${TRAVIS_NODE_VERSION} = '0.12' ]; then
