3b3f0efc95707f069947cd6d32af8b0233683f4e
vcernomschi <vcernomschi@mitocgroup.com>
2016-03-29 16:11:10 +0300

    Fix launching protractor

--- test/bin/posttest.sh
+++ test/bin/posttest.sh
@@ -4,5 +4,5 @@ source $(dirname $0)/_head.sh
 
 ### Start protractor test ###
 if [ ${TRAVIS_NODE_VERSION} != '5.9' ]; then
-  protractor ${__PROTRACTOR_CONFIG_PATH} --troubleshoot true
+  protractor ${__PROTRACTOR_CONFIG_PATH} --troubleshoot
 fi
