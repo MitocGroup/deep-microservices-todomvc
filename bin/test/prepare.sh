62e6dd5706da163bcfb4c1930000c80010077362
delned <rudicov@gmail.com>
2016-04-14 18:36:22 +0300

    Update tests

--- bin/test/prepare.sh
+++ bin/test/prepare.sh
@@ -2,10 +2,9 @@
 
 source $(dirname $0)/_head.sh
 
-npm install -g babel@5.8.x &&\
+pm install -g babel@5.8.x &&\
 npm install -g deepify &&\
 npm install -g jspm@0.16.15 &&\
-jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN &&\
 npm install -g browserify@11.2.x &&\
 npm install -g jscs@2.1.x &&\
 npm install -g mocha@2.3.x &&\
@@ -23,7 +22,8 @@ npm install -g karma-coverage@douglasduteil/karma-coverage#next &&\
 npm install -g karma-verbose-reporter@0.0.x &&\
 npm install -g karma-phantomjs-launcher@0.2.x &&\
 npm install -g karma-ng-html2js-preprocessor@0.2.x &&\
-npm install isparta@3.1.x
+npm install isparta@3.1.x &&\
+jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
 
 #if [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || [ "${__E2E_WITH_PRIVATE_REPO}" = "${E2E_TESTING}" ]; then
 #  npm run protractor-install
