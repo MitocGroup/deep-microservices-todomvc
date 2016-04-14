4bde951b759df832e3548d4a0ae0f48598e6794b
delned <rudicov@gmail.com>
2016-04-14 18:50:54 +0300

    Update tests. Fix PhantomJS version

--- bin/test/phantomjs/install.sh
+++ bin/test/phantomjs/install.sh
@@ -3,11 +3,18 @@
 # Created by vcernomschi on 10/06/2015
 #
 
-#source $(dirname $0)/../_head.sh
+source $(dirname $0)/../_head.sh
 
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
 # Fix for issue: https://github.com/Medium/phantomjs/issues/430#issuecomment-174038299
 npm config set unsafe-perm false
-npm install -g phantomjs@1.9.18
+
+if [ "${__TRAVIS_NODE_MAJOR_VERSION}" = "5" ]; then
+  echo "My version: ${TRAVIS_NODE_VERSION}"
+  npm install -g phantomjs@2.1.3
+else
+  echo "My version: ${TRAVIS_NODE_VERSION}"
+  npm install -g phantomjs@1.9.18
+fi
 npm config set unsafe-perm true
