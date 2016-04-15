d06f243b3087b2a1339861ad90ffbf310071de43
delned <rudicov@gmail.com>
2016-04-15 11:41:42 +0300

    Update tests.

--- bin/test/prepare.sh
+++ bin/test/prepare.sh
@@ -3,7 +3,7 @@
 source $(dirname $0)/_head.sh
 
 npm install -g babel@5.8.x &&\
-npm install -g deepify &&\
+
 npm install -g jspm@0.16.15 &&\
 npm install -g browserify@11.2.x &&\
 npm install -g jscs@2.1.x &&\
@@ -23,6 +23,7 @@ npm install -g karma-verbose-reporter@0.0.x &&\
 npm install -g karma-phantomjs-launcher@0.2.x &&\
 npm install -g karma-ng-html2js-preprocessor@0.2.x &&\
 npm install isparta@3.1.x &&\
+npm install -g deepify &&\
 jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
 
 if [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || [ "${__E2E_WITH_PRIVATE_REPO}" = "${E2E_TESTING}" ]; then
