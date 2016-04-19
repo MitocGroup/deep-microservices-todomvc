65612fb031193e2eb17f0d7243df71256b935c91
delned <rudicov@gmail.com>
2016-04-19 11:02:32 +0300

    Update tests. Fix unset-perms issue

--- bin/test/phantomjs/install.sh
+++ bin/test/phantomjs/install.sh
@@ -8,13 +8,6 @@ source $(dirname $0)/../_head.sh
 echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
 
 # Fix for issue: https://github.com/Medium/phantomjs/issues/430#issuecomment-174038299
-npm config set unsafe-perm true
-
-#if [ "${__TRAVIS_NODE_MAJOR_VERSION}" = "5" ]; then
-#  echo "My version: ${TRAVIS_NODE_VERSION}"
-#  npm install -g phantomjs@2.1.3
-#else
-#  echo "My version: ${TRAVIS_NODE_VERSION}"
-npm install -g phantomjs@1.9.18
-#fi
-npm config set unsafe-perm false
+#npm config set unsafe-perm true
+npm install -g phantomjs@1.9.18 --unsafe-perm
+#npm config set unsafe-perm false
