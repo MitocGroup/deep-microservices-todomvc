2a4ae95f5ece88003cf57ae96166a31ed3bad106
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-04 13:25:48 +0300

    Update postinstall script

--- bin/test/postinstall.sh
+++ bin/test/postinstall.sh
@@ -2,7 +2,7 @@
 #
 # Created by vcernomschi on 10/06/2015
 #
-source $(dirname $0)/../_head.sh
+source $(dirname $0)/_head.sh
 
 echo "Initializing backend"
 cp src/deeploy.example.json src/deeploy.json &&\
