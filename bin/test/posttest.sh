d2f1306a2add9e5a8de6e5be02e1437cc4eed9b9
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-26 13:15:31 +0300

    Update to skeleton

--- bin/test/posttest.sh
+++ bin/test/posttest.sh
@@ -13,12 +13,15 @@ if [ "${TEST_SUITE}" == "$__BACKEND" ] && \
    ([ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || ([ "${__E2E_WITH_PRIVATE_REPO}" = "${E2E_TESTING}" ] && \
    [ ${TRAVIS_BRANCH} = 'stage' ])); then
 
+
   __PROTRACTOR_CONFIG_PATH=$(find ${__SRC_PATH} -name protractor.config.js -type f -maxdepth 4)
+  __DIRNAME_TO_CONFIG="$(dirname "$__PROTRACTOR_CONFIG_PATH")";               
+  __ABS_PATH="$( cd "$__DIRNAME_TO_CONFIG" && pwd )"
 
   ###########################################################################
   ### Check if protractor.config.js exists, otherwise exit with error msg ###
   ###########################################################################
-  if [ ! -f "__PROTRACTOR_CONFIG_PATH" ]; then
+  if [ ! -f "${__ABS_PATH}/protractor.config.js" ]; then
     echo "Protractor config [protractor.config.js] doesn't exist. Please create it and repeat"
     exit 1
   fi
