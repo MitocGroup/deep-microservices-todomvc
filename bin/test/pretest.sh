94f84348606b1bb901fbb5f0aaeedb82a5bed0b6
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-09 11:37:02 +0300

    Add execution permitions to Exec.js

--- bin/test/pretest.sh
+++ bin/test/pretest.sh
@@ -58,6 +58,7 @@ isLocalServerUp () {
 if [ "${TEST_SUITE}" != "frontend" ] &&\
  ([ "${BACKEND_MICROAPP_PATHS}" != "$__NONE" ] || [ "${FRONTEND_MICROAPP_PATHS}" != "$__NONE" ]) &&\
  [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ]; then
+  sudo chmod +x $(npm root -g)/deepify/node_modules/deep-package-manager/lib.compiled/Helpers/Exec.js
   deepify server ${__SRC_PATH} -s & sleep 15 & isLocalServerUp
 else
   echo "Skipping launching deepify server"
