cb11593663bf6e1aecc63ac870c86674910d7d2c
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-09 13:27:37 +0300

    Add timeout to instable test

--- src/deep-todo/tests/e2e/Tests/TaskEditing.spec.js
+++ src/deep-todo/tests/e2e/Tests/TaskEditing.spec.js
@@ -10,6 +10,9 @@ describe('Check that task name can be updated', function() {
 
   it('Adding new task', function() {
     TaskList.addTask('protractor test task1');
+
+    //wait untill all task will be loaded
+    browser.sleep(config.config.jasmineNodeOpts.defaultTimeoutInterval / 10);
   });
 
   it('Updating the task name and verifying that it is saved', function() {
