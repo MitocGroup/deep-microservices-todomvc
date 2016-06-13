351f1c5682a1250ccc56a219dde9b13a0ff5b44d
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-13 11:27:57 +0300

    Optimize dependencies installation

--- bin/test/preinstall.sh
+++ bin/test/preinstall.sh
@@ -10,7 +10,7 @@ source $(dirname $0)/_head.sh
 ####################################################
 (npm list -g babel-cli --depth=0 || npm install -g babel-cli) &&\
 (npm list -g babel-polyfill --depth=0 || npm install -g babel-polyfill) &&\
-(npm list -g babel-preset-es2015 --depth=0 || npm install babel-preset-es2015) &&\
+(npm list -g babel-preset-es2015 --depth=0 || npm install -g babel-preset-es2015) &&\
 (npm list -g deepify@$(npm show deepify version) --depth=0 || npm install -g deepify) &&\
 (npm list -g jspm --depth=0 || npm install -g jspm@0.16.15)  &&\
 (npm list -g browserify --depth=0 || npm install -g browserify@11.2.x) &&\
@@ -36,8 +36,7 @@ source $(dirname $0)/_head.sh
 ###################################################
 (if [ ! -d "node_modules/isparta" ]; then npm install isparta@3.1.x; fi) &&\
 (if [ ! -d "node_modules/sync-exec" ]; then npm install sync-exec@^0.6.x; fi) &&\
-(if [ ! -d "node_modules/fs-extra" ]; then npm install fs-extra@0.x.x; fi) &&\
-(if [ ! -d "node_modules/babel-preset-es2015" ]; then npm link babel-preset-es2015; fi)
+(if [ ! -d "node_modules/fs-extra" ]; then npm install fs-extra@0.x.x; fi)
 
 #############################################################################
 ### Configure jspm and git if we are in CI                                ###
@@ -62,6 +61,7 @@ if [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || [ "${__E2E_WITH_PRIVATE
   ### Install locally, protractor doesn't find babel globally ###
   ###############################################################
   (if [ ! -d "node_modules/babel-cli" ]; then npm link babel-cli; fi) &&\
+  (if [ ! -d "node_modules/babel-preset-es2015" ]; then npm link babel-preset-es2015; fi) &&\
   (if [ ! -d "node_modules/babel-plugin-add-module-exports" ]; then npm install babel-plugin-add-module-exports; fi) &&\
   (if [ ! -d "node_modules/jasmine2-custom-message" ]; then npm install jasmine2-custom-message@0.8.x; fi) &&\
   (if [ ! -d "node_modules/jasmine-utils" ]; then npm install jasmine-utils@0.2.x; fi)
