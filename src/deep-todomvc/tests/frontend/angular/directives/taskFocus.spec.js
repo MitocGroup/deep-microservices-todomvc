8612ea6a7eded9a3cb4f49d10eb550dc017a54a3
Eugene <eistrati@mitocgroup.com>
2016-07-08 09:36:17 -0400

    testing updated structure + unit testing

---
+++ src/deep-todomvc/tests/frontend/angular/directives/taskFocus.spec.js
@@ -0,0 +1,43 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON 07/08/2016 09:23:25
+
+/* global angular */
+/* global inject */
+
+'use strict';
+
+import moduleName from '../../../../frontend/js/app/angular/name';
+
+// @todo: Add more advanced tests
+describe('Directives', () => {
+
+  let element;
+  let compiledElement;
+  let $compile;
+  let $rootScope;
+
+  beforeEach(() => {
+    module('ui.router');
+    angular.mock.module(moduleName);
+  });
+
+  // Store references to $rootScope and $compile
+  // so they are available to all tests in this describe block
+  beforeEach(inject((_$rootScope_, _$compile_) => {
+    $rootScope = _$rootScope_;
+    $compile = _$compile_;
+    element = angular.element('<div data-ng-model="testedModelValue" task-focus div>');
+    $rootScope.testedModelValue = null;
+    $rootScope.$digest();
+
+    // Compile a piece of HTML containing the directive
+    // fire all the watches, so the scope expressions will be evaluated
+    compiledElement = $compile(element)($rootScope);
+  }));
+
+  //@todo - should be added different directive's use cases
+  describe('taskFocus', () => {
+    it('task-focus exists', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
