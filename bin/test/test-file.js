79c4bbcb0ae70416a1256f2f7b670caea1671744
AScripnic <scripnic.alexandru@gmail.com>
2016-12-13 16:17:51 +0200

    Update pretest script

---
+++ bin/test/test-file.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let child_process = require('child_process');
+let path = require('path');
+
+let compileDev = child_process.spawn(`deepify`, ['compile', 'dev', path.join(__dirname, process.argv[2]), '-s']);
+
+
+compileDev.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+compileDev.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+compileDev.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
