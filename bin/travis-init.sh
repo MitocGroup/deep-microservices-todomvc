d8df6c335e9d892a86f2cd2eb9b444fb57c5c3e8
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-09-08 18:37:47 +0300

    #DEEP-TODO integrate reCInk & remove old testing skeleton

---
+++ bin/travis-init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Install global packages
+NPM_BIN=`which npm`
+GLOBAL_DEPS=(
+    deepify
+    recink
+    recink-snyk
+    recink-pagespeed
+    recink-codeclimate
+);
+
+for DEP in ${GLOBAL_DEPS[@]}; do
+    if [ ! -d "$(${NPM_BIN} root -g)/${DEP}" ]; then
+        echo "Installing missing ${DEP}"
+        ${NPM_BIN} install -g ${DEP} || (echo "Failed to install ${DEP}" && exit 1)
+    fi
+done
+
+# Install Chai for test-runner
+npm install chai
