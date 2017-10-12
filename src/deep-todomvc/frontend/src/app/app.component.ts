08427d95abd346fc3842aeb53a2c655d959a7e40
ddimitrioglo <ddimitrioglo@pycoding.biz>
2017-10-12 19:10:28 +0300

    #NG-TODO switch to scss

--- src/deep-todomvc/frontend/src/app/app.component.ts
+++ src/deep-todomvc/frontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ declare const DeepFramework: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   public taskInput: string;
